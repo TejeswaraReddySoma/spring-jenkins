e53fd9482d6abab626ddc19acdccdbe3b22171a4
Gaja Tejeswara Reddy <tejeswarareddygaja@gmail.com>
2022-03-22 11:56:44 +0530

    first commited

---
+++ src/main/java/com/example/demo/SpringBootJenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsApplication {
+
+	Logger logger=LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+	@PostConstruct
+	public void intt()
+	{
+		logger.info("Application started");
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsApplicationTests {
+
+	Logger logger=LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+	@Test
+	public void contextLoads() {
+		logger.info("test executing");
+		assertEquals(true,true);
+	}
+
+}
